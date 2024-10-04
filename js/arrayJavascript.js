2f102567f1ef4d209e4423914335cd9d5dda5d64
cdgalvis <cristiangalviscardona@gmail.com>
2024-10-04 03:03:59 +0200

    Nuevos Ajustes

--- js/arrayJavascript.js
+++ js/arrayJavascript.js
@@ -1,16 +1,35 @@
-var items = localStorage.getItem('itemList')            
-items = items ? items.split(',') : []
 
-console.log('Inicio: ')
-console.log(items)
+// Obteniendo el dato de la clave itemList que esta en LocalStorage
+var items = localStorage.getItem('itemList') 
+// Convertir el dato en un array           
+items = items ? items.split(',') : []
+showItem()
 
 function addItem() {
     let item = document.getElementById('nameItem').value
     if(item){
         items.push(item)
+        showItem()
+    }
+}
+
+function showItem() {
+    document.getElementById('nameItem').value = ''
+
+    let html = ""
+    for (let i = 0; i < items.length ; i++) {
+           html += "<div class='col-6 m-2'>" + items[i] + "</div>" 
+           html += "<div class='col-2 m-2'> <input type='button' class='btn btn-danger' onclick='deleteItem("+ i +")' value='X'> </div>" 
     }
 
-    console.log(items)
+    // Guardamos el dato de la clave itemList que esta en LocalStorage
+    localStorage.setItem('itemList', items) 
+    document.getElementById('ListItems').innerHTML = html
+}
 
-    localStorage.setItem('itemList', items)  
+
+
+function deleteItem(item) {
+
+    showItem()    
 }
